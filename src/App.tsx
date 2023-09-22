f851fe8fdfc15de009d034470cbb1ffa2c5ea886
Nicholas <gianfrancopr08@gmail.com>
2023-09-21 22:25:47 -0500

    Flor con canción.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import './FlorDeEncantados.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const FlorDeEncantados: React.FC<{ brilla: boolean }> = ({brilla}) => {
+    return (
+        <div className={`flor-de-encantados ${brilla ? 'brilla petalo' : ''}`}>
+            <div className="tallo"></div>
+            <div className="hoja izquierda"></div>
+            <div className="hoja derecha"></div>
+            <div className="centro-flor"></div>
+            <div className="petalo uno"></div>
+            <div className="petalo dos"></div>
+            <div className="petalo tres"></div>
+            <div className="petalo cuatro"></div>
+            <div className="petalo cinco"></div>
+            <div className="petalo seis"></div>
+            <div className="petalo siete"></div>
+            <div className="petalo ocho"></div>
+        </div>
+    );
+};
+const App: React.FC = () => {
+    const [playing, setPlaying] = useState(false);
+    const [brilla, setBrilla] = useState(false);
+
+    const handlePlay = () => {
+        setPlaying(true);
+        setBrilla(true); // Activar la animación de brillo cuando se inicia la canción
+        const audio = new Audio('/songs/encantados.mp3');
+        audio.addEventListener('ended', () => {
+            setPlaying(false);
+            setBrilla(false); // Desactivar la animación de brillo cuando la canción termina
+        });
+        audio.play();
+    };
+
+    useEffect(() => {
+        if (playing) {
+            const audio = new Audio('/songs/encantados.mp3');
+            audio.addEventListener('ended', () => {
+                setPlaying(false);
+                setBrilla(false); // Desactivar la animación de brillo cuando la canción termina
+            });
+            audio.play();
+        }
+    }, [playing]);
+
+    return (
+        <div className="app">
+            <div className="button-container">
+                <button onClick={handlePlay} disabled={playing}>
+                    Dale click a la flor :D
+                </button>
+            </div>
+            <FlorDeEncantados brilla={brilla}/>
+            <h1 className="bottom-heading">Para mi hermosa Paulita </h1>
+        </div>
+    );
+};
 
 export default App;
